2ab69ad4951ad6032905d699dcad27441fecdc1e
anukulghoshdev <anukulghosh264@gmail.com>
2023-06-22 16:20:06 +0600

    light dark with localstrage added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,41 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 //import logo
 // import LogoLight from '../assets/img/bg-light';
 // import LogoDark from '../assets/img/bg-dark';
 //import icons
-import { BsFillSunFill, BsMoonFill, BsCheck } from "react-icons/bs";
+import { BsFillSunFill, BsMoonFill } from "react-icons/bs";
 
 const Hero = () => {
   //select html element
   const [theme, setTheme] = useState("light");
 
-  const handleThemeSwitch = () => {
+  // if localstorage is empty then save theme as light
+  useEffect(() => {
+    if (localStorage.getItem("theme") === null) {
+      localStorage.setItem("shopping-cart", JSON.stringify("light"));
+    }
+  }, []);
+
+  useEffect(()=>{
     const html = document.querySelector("html");
-    if (theme === "light") {
+    if (localStorage.getItem("theme") === "dark") {
       setTheme("dark");
       html.classList.add("dark");
     } else {
       html.classList.remove("dark");
       setTheme("light");
+    }
+  }, [theme])
+
+
+  const handleThemeSwitch = () => {
+    if(localStorage.getItem('theme')==='light'){
+      setTheme('dark')
+      localStorage.setItem('theme', ('dark'))
+    }
+    else{
+      setTheme('light')
+      localStorage.setItem('theme', ('light'))
     }
   };
 
